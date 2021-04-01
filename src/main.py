145fefcb148d40115f0005d7557cb64a0aeeca1e
Ezequiel <ezequieljfrias20@gmail.com>
2021-03-31 23:28:26 -0400

    Casi terminada API

--- src/main.py
+++ src/main.py
@@ -8,7 +8,11 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User,Sale
+from random import randint
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity)
 #from models import Person
 
 app = Flask(__name__)
@@ -30,16 +34,114 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
-
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+# Endpoint para ver todos los usuarios
+@app.route('/users', methods=['GET'])
+def handle_users():
+    users=User.query.all()
+    response_body=[]
+    for user in body:
+        response_body.append(user.serialize())
 
     return jsonify(response_body), 200
 
+#Endpoint para solicitar los datos de un usuario
+@app.route('/users/<int:id>', methods=['GET'])
+def handle_one_user(id):
+    user=User.query.get(id)
+    if user is None:
+        return "NO EXISTE",404
+    else:
+        return jsonify(user.serialize()), 202
+
+#Endpoint para crear un usuario
+@app.route('/users', methods=['POST'])
+def add_new_user():
+    body= request.get_json()
+    #validaciones de body para campos obligatorios 
+    if isinstance (body,dict):
+        if body is None:
+            raise APIException("Please specify the request body as a json object", status_code=400)
+        if 'email' not in body:
+            raise APIException("You need to specify the name", status_code=400)
+        if 'name' not in body:
+            raise APIException("You need to specify the name", status_code=400)
+        if 'password' not in body:
+            raise APIException("You need to specify the name", status_code=400)
+    else: return "error in body, is not a dictionary"
+    user1 = User.create(
+        email=body['email'],
+        name=body['name'],
+        password=body['password'])
+    return user1.serialize(), 200
+
+#endpoint para el login de un usuario 
+@app.route("/login", methods=["POST"])
+def handle_login():
+    """ 
+        check password for user with email = body['email']
+        and return token if match.
+        comprobar la contraseña del usuario con email = body ['email']
+         y devolver el token si coincide.
+    """
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request / Falta JSON en la solicitud"}), 400
+    params = request.get_json()
+    email = params.get('email', None)
+    password = params.get('password', None)
+    if not email:
+        return jsonify({"msg": "Missing email parameter / Falta el parametro de correo electronico"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter / Falta el parametro de contrasena"}), 400
+    user = User.query.filter_by(email=email).one_or_none()
+    if not user:
+        return jsonify({"msg": "User does not exist / El usuario no existe"}), 404
+    if user.check_password(password):
+        jwt= create_access_token(identity=user.id)
+        ret = user.serialize()
+        ret["jwt"]=jwt
+        return jsonify(ret), 200
+    else:
+        return jsonify({"msg": "Bad credentials / Credenciales incorrectas"}), 401
+
+#endpoint para crear una venta
+@app.route('/sales', methods=['POST'])
+def add_new_sale():
+    body= request.get_json()
+    #validaciones de body para campos obligatorios
+    if isinstance (body,dict):
+        if body is None:
+            raise APIException("Please specify the request body as a json object", status_code=400)
+        if 'date' not in body:
+            raise APIException("You need to specify date", status_code=400)
+        if 'description' not in body:
+            raise APIException("You need to specify description", status_code=400)
+        if 'money_USD' not in body:
+            raise APIException("You need to specify money", status_code=400)
+        if 'user_id' not in body:
+            raise APIException("You need to specify the id user", status_code=400)
+
+    else: return "error in body, is not a dictionary"
+    task1 = Task.create(
+        date=body['date'],
+        user_id=body['user_id'],
+        description=body['description'],
+        money_USD=body['money_USD'])
+    return task1.serialize(), 200
+
+@app.route('/sales/<int:id>',methods=["GET"])
+def all_sales(id):
+    user=User.query.get(id)
+    if user is None:
+        return "NO EXISTE",404
+    if id is None:
+        raise APIException('You need to specify an existing user', status_code=400)
+    sales = Sale.query.filter_by(user_id=id)
+    response_body= []
+    for sale in sales:
+        response_body.append(sale.serialize())
+    return jsonify(response_body),200
+
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
-    PORT = int(os.environ.get('PORT', 3000))
+    PORT = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,19 +1,78 @@
 from flask_sqlalchemy import SQLAlchemy
+import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from base64 import b64encode
 
 db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name= db.Column(db.String(120),nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    hashed_password = db.Column(db.String(80), unique=False, nullable=False)    
+    salt=db.Column(db.String(200),nullable=False)
+    sales=db.relationship("Sale",backref="user")
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def __init__(self,body):
+        self.name=body['name']
+        self.email=body['email']
+        self.salt=b64encode(os.urandom(4)).decode("utf-8")
+        self.hashed_password=set_password(body['password'])
 
+    @classmethod
+    def create(cls,**kwargs):
+        new_user=cls(kwargs)
+        db.session.add(new_user)
+        db.session.commit()
+        return new_user
+
+    def set_password(self,password):
+        return generate_password_hash(
+            f"{password}{self.salt}"
+        )
+
+    def check_password(self, password):
+        print(f"este es el password:{password}")
+        return check_password_hash(
+            self.hashed_password,
+            f"{password}{self.salt}"
+        )
+        
     def serialize(self):
         return {
             "id": self.id,
+            "name":self.name,
             "email": self.email,
+            # do not serialize the password, its a security breach
+        }
+    
+class Sale(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date= db.Column(db.Integer(120),nullable=False)
+    description=db.Column(db.String(120), unique=True, nullable=False)
+    money_USD = db.Column(db.String(120), unique=True, nullable=False)
+
+    user_id=db.Column(db.Integer, db.ForeignKey('user.id'))
+
+
+    def __init__(self,body):
+        self.date=body['date']
+        self.description=body['description']
+        self.money_USD=body['money_USD']
+
+    @classmethod
+    def create(cls,**kwargs):
+        new_sale=cls(kwargs)
+        db.session.add(new_sale)
+        db.session.commit()
+        return new_sale
+
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "date":self.date,
+            "description": self.description,
+            "money_USD"=self.money_USD
             # do not serialize the password, its a security breach
         }
